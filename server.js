48e6ecc75ea67a1d823f197b5907252ae47e9544
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-06 22:15:44 +0530

    Creating Room On Landing

--- server.js
+++ server.js
@@ -5,4 +5,22 @@ const server = require('http').Server(app)
 
 const io = require('socket.io')(server)
 
+const { v4: uuidV4 } = require('uuid')
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    const roomId = uuidV4()
+    res.redirect(`/${roomId}`)
+})
+
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+
 server.listen(3000)
